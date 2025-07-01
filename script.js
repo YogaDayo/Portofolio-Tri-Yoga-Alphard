f9fa3f075e16a3490b6f0769863dd5a54ff5fb7c
YogaDayo <yogaalphard@gmail.com>
2025-07-01 19:45:15 +0700

    Update Portofolio

---
+++ script.js
@@ -0,0 +1,52 @@
+// toggle menu
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("show");
+}
+
+// About Section
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return rect.top <= window.innerHeight && rect.bottom >= 0;
+}
+
+// animation when scroll
+function handleScrollAnimation() {
+  const aboutText = document.getElementById("aboutText");
+  const aboutImage = document.getElementById("aboutImage");
+
+  if (isInViewport(aboutText)) {
+    aboutText.classList.add("show-text");
+  }
+  if (isInViewport(aboutImage)) {
+    aboutImage.classList.add("show-image");
+  }
+}
+
+// Trigger saat scroll
+window.addEventListener("scroll", handleScrollAnimation);
+window.addEventListener("load", handleScrollAnimation);
+
+// projects
+function animateCards() {
+  const cards = document.querySelectorAll(".project-card");
+  cards.forEach((card) => {
+    if (isInViewport(card)) {
+      card.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", animateCards);
+window.addEventListener("load", animateCards);
+
+// contacts
+function animateContact() {
+  const contactSection = document.querySelector(".contact");
+  if (isInViewport(contactSection)) {
+    contactSection.classList.add("show");
+  }
+}
+
+window.addEventListener("scroll", animateContact);
+window.addEventListener("load", animateContact);
